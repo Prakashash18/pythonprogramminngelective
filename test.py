ffa91c28bda538535f21fe61557fd1f0f5b83f9c
prakashash18 <prakashash18@gmail.com>
2022-01-27 09:40:07 +0800

    Create test.py

---
+++ test.py
@@ -0,0 +1,27 @@
+#get inputs
+a = input()
+b = input()
+c = input()
+
+#add a,b,c and print the result as an integer
+#first one done for you !
+print(int(a)+int(b)+int(c))
+
+#add a,b,c and print the result as a float
+print(float(a) + float(b) + float(c))
+
+#print the result of a to the power of 8
+print(int(a)*1e8)
+
+#print the result of a divide by b as int
+print(int(a)/int(b))
+
+#print the remainder of a divided by b
+print(int(a)%int(b))
+
+#print the answer to check if a is greater than b
+print(int(a)> int(b))
+
+#print the answer to check if b is greater than c
+print(int(b)>int(c))
+
